ba17dc4885a910dceb3f6d2e1c38750fa61b107e
kimnapadon-05 <kimnapadon1412@gmail.com>
2026-01-29 09:26:12 +0700

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,5 +1,5 @@
 <?php
-// TODO: แก้ข้อมูลเชื่อม DB ให้ตรงกับของจริง
+
 $db_host = "localhost";
 $db_user = "your_username";
 $db_pass = "your_password";
@@ -7,7 +7,6 @@ $db_name = "your_database";
 
 $conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
 
-// TODO: Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
